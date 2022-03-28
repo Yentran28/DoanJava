70e05c967c82a89fe630b8c773b1d3193521d8a5
Yentran28 <85753110+Yentran28@users.noreply.github.com>
2022-03-28 18:15:43 +0700

    Add files via upload

---
+++ doan/README.txt
@@ -0,0 +1,6 @@
+Môn Đồ Án Cơ Sở
+Làm về game: Game nối hình giống nhau
+Thời gian: 12 tuần
+Công nghệ sử dụng: Apache NetBeans/JavaFx
+Số lượng người tham gia: 01
+Ngôn ngữ lập trình: Java
---
+++ doan/src/controller/MainFrame.java
@@ -0,0 +1,173 @@
+package controller;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+public class MainFrame extends JFrame implements ActionListener, Runnable {
+        private int MAX_TIME =300 ;
+	public int time = MAX_TIME;
+	private int row = 8;
+	private int col = 8;
+	private int width = 1200;
+	private int height = 1000;
+        public JLabel lbScore;
+	private JProgressBar progressTime;
+	private JButton btnNewGame;
+	private ButtonEvent graphicsPanel;
+	private JPanel mainPanel;
+        private boolean pause=false;
+        private boolean resume= false;
+	public MainFrame() {
+		add(mainPanel = createMainPanel());
+		setTitle("Nối Hình");
+                
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(width, height);
+		setLocationRelativeTo(null);
+		setVisible(true);
+                
+	}
+
+	private JPanel createMainPanel() {
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(createGraphicsPanel(), BorderLayout.CENTER);
+                panel.add(createControlPanel(), BorderLayout.EAST);
+                //panel.setBackground(Color.red);
+		return panel;
+	}
+
+	private JPanel createGraphicsPanel() {
+		graphicsPanel = new ButtonEvent(this, row, col);
+		JPanel panel = new JPanel(new GridBagLayout());
+		panel.setBackground(Color.BLACK);
+		panel.add(graphicsPanel);
+		return panel;
+	}
+        private JPanel createControlPanel() {
+                //tạo JLable lblScore với giá trị ban đầu là 0
+		lbScore = new JLabel("0");
+		progressTime = new JProgressBar(0, 1);
+		progressTime.setValue(100);
+
+		//tạo Panel chứa Score và Time
+
+		JPanel panelLeft = new JPanel(new GridLayout(2, 1, 5, 5));
+		panelLeft.add(new JLabel("Điểm:"));
+		panelLeft.add(new JLabel("Thời Gian:"));
+
+		JPanel panelCenter = new JPanel(new GridLayout(2, 1, 5, 5));
+		panelCenter.add(lbScore);
+		panelCenter.add(progressTime);
+
+		JPanel panelScoreAndTime = new JPanel(new BorderLayout(5, 0));
+		panelScoreAndTime.add(panelLeft, BorderLayout.WEST);
+		panelScoreAndTime.add(panelCenter, BorderLayout.CENTER);
+
+		// tạo Panel mới chứa panelScoreAndTime và nút New Game
+		JPanel panelControl = new JPanel(new BorderLayout(10, 10));
+		panelControl.setBorder(new EmptyBorder(10, 3, 5, 3));
+		panelControl.add(panelScoreAndTime, BorderLayout.CENTER);
+		panelControl.add(btnNewGame = createButton("Làm Mới"),
+				BorderLayout.PAGE_END);
+
+                
+
+		// Set BorderLayout để panelControl xuất hiện ở đầu trang
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.setBorder(new TitledBorder("Chúc may mắn"));
+		panel.add(panelControl, BorderLayout.PAGE_START);
+	
+		return panel;
+	}
+      
+
+	// create a button
+	private JButton createButton(String buttonName) {
+		JButton btn = new JButton(buttonName);
+		btn.addActionListener(this);
+                btn.setBackground(Color.ORANGE);
+		return btn;
+	}
+
+	public void newGame() {
+		time = MAX_TIME;
+		graphicsPanel.removeAll();
+		mainPanel.add(createGraphicsPanel(), BorderLayout.CENTER);
+		mainPanel.validate();
+		mainPanel.setVisible(true);
+		lbScore.setText("0");
+	}
+        @Override
+	public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == btnNewGame) {
+                
+			             showDialogNewGame("Bạn vẫn còn trong game. Bạn có muốn chơi màn mới?", "Cảnh Báo",0);
+		}
+	}
+        
+
+	@Override
+	public void run() {
+            while (true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			progressTime.setValue((int) ((double) time / MAX_TIME * 10));
+		}
+	}
+
+        public boolean isResume() {
+            return resume;
+        }
+
+        public void setResume(boolean resume) {
+            this.resume = resume;
+        }
+        
+        public boolean isPause() {
+            return pause;
+        }
+
+        public void setPause(boolean pause) {
+            this.pause = pause;
+         }   
+       
+
+	public void showDialogNewGame(String message, String title, int t) {
+                pause=true;
+                resume=false;
+		int select = JOptionPane.showOptionDialog(null, message, title,
+				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+				null, null);
+		if (select == 0) {
+                    pause=false;
+			newGame();
+		} else {
+			if(t==1){
+                            System.exit(0);
+                        }else{
+                            resume=true;
+                        }
+		}
+	}
+}
